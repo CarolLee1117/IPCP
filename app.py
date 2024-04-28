02fed205f9d89a188a5089e785f851f205aa314c
giraffe <clee704202@gmail.com>
2024-04-28 21:18:29 +0800

    update: app.py and README.md

---
+++ app.py
@@ -0,0 +1,14 @@
+'''
+做一個簡單的伺服器
+'''
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
