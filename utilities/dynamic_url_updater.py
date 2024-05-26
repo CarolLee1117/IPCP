54686f1b9b896ae6217773d3fcdcfc30eb416658
giraffe <clee704202@gmail.com>
2024-05-26 22:54:40 +0800

    feat: Implement utilities/dynamic_url_updater.py

---
+++ utilities/dynamic_url_updater.py
@@ -0,0 +1,62 @@
+'''
+使用
+```bash
+ngrok http [port_number] --log=stdout > ./ngrok.log &
+```
+便會在當下的目錄中產生一個 ngrok.log 檔案，裡面會有 ngrok 的 log 訊息
+'''
+import os
+from time import sleep
+
+from dotenv import load_dotenv
+from email_sender import EmailSender
+
+
+load_dotenv()
+
+
+class dynamicUrlUpdater:
+    '''
+    dynamicUrlUpdater class
+    '''
+    def restart_ngrok(self):
+        '''
+        kill all ngrok process and restart ngrok
+        '''
+        os.system('killall ngrok')
+        os.system('ngrok http 5000 --log=stdout > ./ngrok.log &')
+
+    def get_ngrok_url(self):
+        '''
+        analyze the log file and get the url from "url=" statement.
+        Then send the url with email_sender.EmailSender
+        '''
+        my_sap = os.getenv('SAP')
+        with open('./ngrok.log', 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            for line in lines:
+                if 'started tunnel' in line:
+                    url = line.split()[-1].strip()
+                    msg = EmailSender(
+                        src='clee704202@gmail.com',
+                        sap=my_sap
+                    ).send_email(
+                        subject='ngrok url 更新通知',
+                        dst='freddy.wang@orangeapple.co',
+                        context=f'ngrok url: {url}',
+                    )
+                    return msg
+                
+
+if __name__ == '__main__':
+    duu = dynamicUrlUpdater()
+    interval = 300 # 5 minutes
+    while True:
+        try:
+            duu.restart_ngrok()
+            sleep(3) # wait for ngrok to start        
+            print(duu.get_ngrok_url())
+            sleep(interval)
+        except KeyboardInterrupt:
+            break
+    os.system('killall ngrok')
